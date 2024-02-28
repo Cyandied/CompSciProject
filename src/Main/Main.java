0ae1dffbc821d697d36b3c4824bd64752f0484cc
Cyandied <awesome.pie753@gmail.com>
2024-02-28 12:57:16 +0100

    Prepare for JavaFX development
    Even though JavaFX application implicitly calls a main function when launch() is run, this will not work if project is built. To circumvent this problem all together we can keep a Main class with only a main function that calls some custom start function for the JavaFXApp class which calls the launch() method

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,11 @@
 package Main;
 
+import UIElems.JavaFXApp;
+
 public class Main {
     public static void main(String[] args) {
+        JavaFXApp app = new JavaFXApp();
+        app.costumStart();
         RobotsGame game = new RobotsGame(10, 10); // Initialize the game with a 5x5 world
         game.startGameLoop();
     }
---
+++ src/UIElems/JavaFXApp.java
@@ -0,0 +1,24 @@
+package UIElems;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXApp extends Application {
+
+    public void init() {
+
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public void stop(){
+
+    }
+
+    public void costumStart(){
+        launch();
+    }
+
+}
